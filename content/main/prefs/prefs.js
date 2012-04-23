311c0b27bcd316b7cef58af0faf89032b658832f
Dave Stewart <dave@davestewart.co.uk>
2012-04-23 18:37:40 +0100

    Removed xjsfl reference

--- content/main/prefs/prefs.js
+++ content/main/prefs/prefs.js
@@ -1,38 +1,37 @@
 // ------------------------------------------------------------------------------------------------------------------------
 //
-//  ██████             ████       
-//  ██  ██             ██         
-//  ██  ██ ████ █████  ██   █████ 
-//  ██████ ██   ██ ██ █████ ██    
-//  ██     ██   █████  ██   █████ 
-//  ██     ██   ██     ██      ██ 
-//  ██     ██   █████  ██   █████ 
+//  ██████             ████
+//  ██  ██             ██
+//  ██  ██ ████ █████  ██   █████
+//  ██████ ██   ██ ██ █████ ██
+//  ██     ██   █████  ██   █████
+//  ██     ██   ██     ██      ██
+//  ██     ██   █████  ██   █████
 //
 // ------------------------------------------------------------------------------------------------------------------------
 // Prefs
 
 	// --------------------------------------------------------------------------------
 	// debug
-	
+
 		//var log		= ko.logging.getLogger("pref-jsfl");
 		//log.setLevel(ko.logging.LOG_DEBUG);
 		//log.setLevel(ko.logging.LOG_INFO);
-	
+
 	// --------------------------------------------------------------------------------
 	// globals
-	
+
 		if(parent && parent.opener)
 		{
 			var clear		= window.clear || parent.opener.clear || function(){ };
 			var trace		= window.trace || parent.opener.trace || function(){ };
 			var ko			= parent.opener.ko;
-			var xjsfl		= parent.opener.xjsfl;
 			var xjsflLib	= parent.opener.xjsflLib;
 		}
-	
+
 	// --------------------------------------------------------------------------------
 	// variables
-	
+
 		var win			= parent && parent.opener ? parent.opener : window;
 		var ui			= new xjsflLib.UIManager(this);
 		var autocode	= win.autocode;
@@ -53,43 +52,42 @@
 			'placesFileTypes',
 			'placesDefaultFormats',
 		];
-		
+
 	// --------------------------------------------------------------------------------
 	// handlers
-	
+
 		function onLoad()
 		{
 			// load prefs
 				ui.loadGroup(elements);
-				
+
 			// update filetypes with default if they don't exist yet
 				var fileTypes = document.getElementById('placesFileTypes');
 				if(fileTypes.value == '')
 				{
 					fileTypes.value = autocode.places.settings.getDefaultPrefFileTypes();
 				}
-				
+
 			// tell parent we've loaded
 				parent.hPrefWindow.onpageload();
 		}
-		
+
 		function OnPreferencePageOK()
 		{
 			// save prefs
 				ui.saveGroup();
-			
+
 			// update
 				window.setTimeout(function(){autocode.initialize.call(autocode); }, 250);
-				
+
 			// return
 				return true;
 		}
-		
+
 		function help()
 		{
 			ko.open.URI('chrome://autocode/content/main/help/index.html', 'browser');
 		}
-		
+
 	// --------------------------------------------------------------------------------
 	// methods
-	
