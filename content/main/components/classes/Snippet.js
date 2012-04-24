749980583d867173c1f27ecc619f3bd3cacfb854
Dave Stewart <dave@davestewart.co.uk>
2012-04-24 22:40:31 +0100

    Corrected spaces vs tabs

--- content/main/components/classes/Snippet.js
+++ content/main/components/classes/Snippet.js
@@ -2,11 +2,11 @@
 //
 //  ██████       ██                    ██
 //  ██                                 ██
-//  ██     █████ ██ █████ █████ █████ █████ 
-//  ██████ ██ ██ ██ ██ ██ ██ ██ ██ ██  ██   
-//      ██ ██ ██ ██ ██ ██ ██ ██ █████  ██   
-//      ██ ██ ██ ██ ██ ██ ██ ██ ██     ██   
-//  ██████ ██ ██ ██ █████ █████ █████  ████ 
+//  ██     █████ ██ █████ █████ █████ █████
+//  ██████ ██ ██ ██ ██ ██ ██ ██ ██ ██  ██
+//      ██ ██ ██ ██ ██ ██ ██ ██ █████  ██
+//      ██ ██ ██ ██ ██ ██ ██ ██ ██     ██
+//  ██████ ██ ██ ██ █████ █████ █████  ████
 //                  ██    ██
 //                  ██    ██
 //
@@ -34,15 +34,15 @@ autocode.classes.Snippet.prototype =
 	// ----------------------------------------------------------------------------------------------------
 
 		name		:'',
-		            
+
 		path		:'',
-		            
+
 		template	:'',
-		            
+
 		value		:'',
-		
+
 		exists		:false,
-		            
+
 	// ----------------------------------------------------------------------------------------------------
 	// methods
 	// ----------------------------------------------------------------------------------------------------
@@ -55,14 +55,14 @@ autocode.classes.Snippet.prototype =
 		load:function(path)
 		{
 			// toolbox path
-				var devPath		= 'E:/05 - Commercial Projects/xJSFL/3 - development/Komodo Extensions/Komodo AutoCode Extension/tools/';
+				var devPath		= 'E:/05 - Commercial Projects/xJSFL/3 - development/Komodo Extensions/AutoCode/tools/';
 				var toolbox		= ko.toolbox2.getExtensionToolbox("autocode@xjsfl.com");
 				toolbox			= toolbox ? (toolbox.path + '/').replace(/\\/g, '/') + '/' : devPath;
 
 			// snippet path
 				var fullPath	= toolbox + 'Places/' + path + '.komodotool';
 				var file		= new xjsflLib.File(fullPath);
-				
+
 			// populate snippet
 				if(file.exists)
 				{
@@ -70,22 +70,22 @@ autocode.classes.Snippet.prototype =
 						var contents	= file.read();
 						var json		= xjsflLib.JSON.decode(contents);
 						var value		= json.value;
-						
+
 					// update class properties
 						this.exists		= true;
 						this.path		= path;
 						this.name		= path.split('/').pop();
 						this.template	= value.join('\n');
 						this.value		= this.template.replace(/!@#\w+/g, '');
-						
+
 					// return
 						return true;
 				}
-				
+
 			// return
 				return false;
 		},
-		
+
 		/**
 		 * Populates the snippet value with data
 		 * @param	{Object}	data	An object of properties
@@ -100,21 +100,21 @@ autocode.classes.Snippet.prototype =
 			}
 			return this;
 		},
-		
+
 	// ----------------------------------------------------------------------------------------------------
 	// utilities
 	// ----------------------------------------------------------------------------------------------------
 
 		hasAttribute:function()
 		{
-			
+
 		},
-		
+
 		getStringAttribute:function()
 		{
-			
+
 		},
-		
+
 		toString:function()
 		{
 			return '[object Snippet path="' +this.path+ '" exists="' +this.exists+ '"]';
